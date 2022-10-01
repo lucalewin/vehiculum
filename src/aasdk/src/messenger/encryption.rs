8f51809aae39d59d3798ba322eca061141858120
Luca Lewin <luca.lewin23@gmail.com>
2022-10-01 16:48:42 +0200

    remove unused code

--- src/aasdk/src/messenger/encryption.rs
+++ src/aasdk/src/messenger/encryption.rs
@@ -1,21 +1,12 @@
-use bitflags::bitflags;
+// use crate::error::Error;
 
-use crate::error::Error;
+// pub trait ICryptor {
+//     fn init(&self);
+//     fn do_handshake(&self) -> Result<bool, Error>;
 
-bitflags!{
-    pub struct EncryptionType: u8 {
-        const PLAIN = 0;
-        const ENCRYPTED = 1 << 3;
-    }
-}
+//     fn encrypt(&self, output: &mut [u8], data: &[u8]) -> Result<usize, Error>;
+//     fn decrypt(&self, output: &mut [u8], data: &[u8]) -> Result<usize, Error>;
 
-pub trait ICryptor {
-    fn init(&self);
-    fn do_handshake(&self) -> Result<bool, Error>;
-
-    fn encrypt(&self, output: &mut [u8], data: &[u8]) -> Result<usize, Error>;
-    fn decrypt(&self, output: &mut [u8], data: &[u8]) -> Result<usize, Error>;
-
-    fn read_handshake_buffer(&self) -> Result<Vec<u8>, Error>;
-    fn write_handshake_buffer(&self, data: &[u8]) -> Result<usize, Error>;
-}
+//     fn read_handshake_buffer(&self) -> Result<Vec<u8>, Error>;
+//     fn write_handshake_buffer(&self, data: &[u8]) -> Result<usize, Error>;
+// }
